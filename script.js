dfbcfa7bcaddb703b30806dcbc4f555afab432e1
v-adhithyan <aavispeaks@gmail.com>
2019-08-06 08:22:53 +0530

    Save selection to localstorage

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-function getSelection(info, tab) {
-    console.log("Word " + info.selectionText + " was clicked.");
-    chrome.tabs.create({
-        url: "http://www.google.com/search?q=" + info.selectionText
+let DELIMITER = "HIGHDELIMITER;,"
+
+function setSelection(info, tab) {
+    let currentTab = tab.url;
+    chrome.storage.local.set({ currentTab: info.selectionText }, function () {
+        alert("Saved");
     });
 }
+
 chrome.contextMenus.create({
     title: "Highlight: %s",
     contexts: ["selection"],
-    onclick: getSelection
+    onclick: setSelection
 });
